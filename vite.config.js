8ca516a77cb9a0f8a3a3f422976c1c1e02c2f44f
Sanganabasava M <sanganabasava.m@refex.co.in>
2025-04-03 15:52:43 +0530

    Adedd vercel config file

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [
-    react({
-      include: ['**/*.js', '**/*.jsx'], // Allow JSX in .js files
-    }),
-  ],
+  plugins: [react()],
+  base: '/',
+  server: {
+    historyApiFallback: true,
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  preview: {
+    historyApiFallback: true,
+  }
 });
