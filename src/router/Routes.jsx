8dc812b7d97f34806a1dfa2d3c41f7d1df9140cd
Sanganabasava M <sanganabasava.m@refex.co.in>
2025-04-03 17:09:54 +0530

    resolved erorrs which are related to file names

--- src/router/Routes.jsx
+++ src/router/Routes.jsx
@@ -2,10 +2,10 @@ import { Routes, Route } from "react-router-dom";
 import EntryPage from "../pages/EntryPage";
 import ProductList from "../pages/ProductList";
 import ProductDetail from "../pages/ProductDetail";
-import About from "../pages/About"
+import AboutPage from "../pages/AboutPage"
 import ContactPage from "../pages/ContactPage"
 import NotificationPage from "../pages/NotificationPage"
-import Terms from "../pages/TermsPage"
+import TermsPage from "../pages/TermsPage"
 import CartPage from "../pages/CartPage"
 import CheckoutPage from "../pages/CheckoutPage"
 import SettingsPage from "../pages/SettingsPage"
@@ -18,10 +18,10 @@ const AppRoutes = () => {
         <Route index element={<EntryPage />} /> 
         <Route path="/products-list" element={<ProductList />} />
          <Route path="/products/:id" element={<ProductDetail />} />
-         <Route path="/about" element={<About />} />
+         <Route path="/about" element={<AboutPage />} />
          <Route path="/contact" element={<ContactPage />} />
          <Route path="/notifications" element={<NotificationPage />} />
-         <Route path="/terms" element={<Terms />} />
+         <Route path="/terms" element={<TermsPage />} />
          <Route path="/cart" element={<CartPage />} />
          <Route path="/settings" element={<SettingsPage />} />
          <Route path="/checkout" element={<CheckoutPage />} />
